4885ef05b3b20b352bee1b670b7fa7a27038416e
zhangxin <zhangxin0518@outlook.com>
2019-06-12 11:16:29 +0800

    Update operator.v

--- operator.v
+++ operator.v
@@ -9,6 +9,24 @@
 //7) 移位运算符(<<,>>) 
 //8) 拼接运算符({ })
 
-
+// Verilog关键字
+//----------------------------------------
+always， and， assign，begin，buf，bufif0，
+bufif1，case，casex，casez，cmos，deassign， 
+default，defparam，disable，edge，else，end，
+endcase，endmodule，endfunction，endprimitive, 
+endspecify, endtable， endtask， event， for，
+force， forever， fork， function，highz0，
+highz1, if，initial, inout, input，integer，
+join,large，macromodule，medium，module， nand，
+negedge，nmos，nor，not，notif0，notifl, or, 
+output, parameter, pmos, posedge, primitive, 
+pull0, pull1, pullup, pulldown, rcmos, reg, 
+releses, repeat, mmos, rpmos, rtran, rtranif0,
+rtranif1,scalared,small，specify，specparam，
+strength，strong0, strong1, supply0, supply1, 
+table, task, time, tran, tranif0, tranif1, tri, 
+tri0, tri1, triand, trior， trireg，vectored，
+wait，wand，weak0，weak1，while， wire，wor, xnor，xor
 
 
